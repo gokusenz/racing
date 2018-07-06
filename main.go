7131cbb33f7f5699eb3676e849ff93656b118c5d
pallat <yod.pallat@gmail.com>
2018-07-06 21:15:36 +0700

    fix race with sync.WaitGroup

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	fmt.Println(getNumber(5))
@@ -8,9 +11,17 @@ func main() {
 
 func getNumber(n int) int {
 	var i int
+	// Initialize a waitgroup variable
+	var wg sync.WaitGroup
+	// `Add(1) signifies that there is 1 task that we need to wait for
+	wg.Add(1)
 	go func() {
 		i = n
+		// Calling `wg.Done` indicates that we are done with the task we are waiting fo
+		wg.Done()
 	}()
-
+	// `wg.Wait` blocks until `wg.Done` is called the same number of times
+	// as the amount of tasks we have (in this case, 1 time)
+	wg.Wait()
 	return i
 }
--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGet(t *testing.T) {
 	i := getNumber(5)
-	if i != 0 {
-		t.Error("it should be 0 but got", i)
+	if i != 5 {
+		t.Error("it should be 5 but got", i)
 	}
 }
