2468470b1f0bc5129ac9422193955e52b6e4b125
pallat <yod.pallat@gmail.com>
2018-07-06 21:16:39 +0700

    fix race with chan

--- main.go
+++ main.go
@@ -8,9 +8,14 @@ func main() {
 
 func getNumber(n int) int {
 	var i int
+	// Create a channel to push an empty struct to once we're done
+	done := make(chan struct{})
 	go func() {
 		i = n
+		// Push an empty struct once we're done
+		done <- struct{}{}
 	}()
-
+	// This statement blocks until something gets pushed into the `done` channel
+	<-done
 	return i
 }
